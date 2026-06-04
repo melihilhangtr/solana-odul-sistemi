ca42aff5d2206a6f2b9ecbeefb4e2b510bd955b9
melihilhangtr <melihilhangtr@gmail.com>
2026-06-05 00:31:49 +0300

    Update drizzle.config.ts

--- lib/db/src/drizzle.config.ts
+++ lib/db/src/drizzle.config.ts
@@ -1,10 +1,9 @@
-import { defineConfig } from 'drizzle-kit';
+import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: "./lib/db/src/schema/*.ts", 
-  out: './drizzle',
-  dialect: 'postgresql',
+  schema: "./lib/db/src/schema/*.ts",
+  dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL as string,
   },
 });
